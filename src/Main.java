cac959c043ed6e11b5f513617acdb349cdf07294
Kymane Llewellyn <codesocietyclub@gmail.com>
2018-01-30 13:30:12 -0500

    First commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+//        IntStack intStack = new IntStack();
+//        if(intStack.isFull()){
+//            intStack.push(2);
+//            intStack.push(4);
+//            intStack.push(6);
+//            intStack.push(8);
+//
+//        }
+//
+//        System.out.println(intStack.pop());
+//        System.out.println(intStack.pop());
+
+        Person person1 = new Person ("umair", "123");
+        Person person2 = new Person ("ali", "321");
+
+        PersonStack stack = new PersonStack();
+
+        stack.push(person1);
+        stack.push(person2);
+
+
+        System.out.println(stack.pop().toString());
+        System.out.println(stack.pop().toString());
+    }
+
+}
